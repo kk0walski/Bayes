88b5f1e2b81ff2830d787c263411dd952794c10c
karol <karol.kowalski95@gmail.com>
2019-03-18 23:14:26 +0100

    Poprawki w GaussianNB

--- models/GaussianBayes.py
+++ models/GaussianBayes.py
@@ -5,14 +5,16 @@ from sklearn.model_selection import train_test_split
 class GaussianBayes:
 
     def fit(self, X, Y, test_size=0.5):
-        a_values = [-0.1, -0.05, -0.03, -0.02, -0.01, -0.005, 0, 0.005, 0.01, 0.02, 0.03, 0.05, 0.1]
-        b_values = [-0.1, -0.05, -0.03, -0.02, -0.01, -0.005, 0, 0.005, 0.01, 0.02, 0.03, 0.05, 0.1]
-        X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=test_size, random_state=0)
+        a_values = [-0.5, -0.3, -0.2, -0.1, -0.05, -0.03, -0.02, -0.01, -0.005, -0.003, -0.002, 0, 0.005, 0.01, 0.02, 0.03, 0.04, 0.05, 0.1, 0.2, 0.3, 0.5]
+        b_values = [-0.5, -0.3, -0.2, -0.1, -0.05, -0.03, -0.02, -0.01, -0.005, -0.003, -0.002, 0, 0.005, 0.01, 0.02, 0.03, 0.04, 0.05, 0.1, 0.2, 0.3, 0.5]
+        X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=test_size)
         error_best, best_a, best_b, errors = self.modelSelection(X_train, X_test, y_train, y_test, a_values,b_values)
         self.p_y = self.estimate_a_priori(y_train)
         self.summaries = self.summarizeByClass(X_train, y_train, best_a, best_b)
         print(best_a)
         print(best_b)
+        print(error_best)
+
 
     def modelSelection(self, Xtrain, Xval, Ytrain, Yval, aValues, bValues):
         errors = np.zeros((len(aValues), len(bValues)))
@@ -53,14 +55,14 @@ class GaussianBayes:
             separated[classes[i]].append(vector)
         return separated
 
-    def mean(self, numbers):
-        return np.mean(numbers)+0.02
+    def mean(self, numbers, a, b):
+        return np.mean(numbers)+a+b-0.002
 
-    def stdev(self, numbers):
-        return np.std(numbers)+0.01
+    def stdev(self, numbers, b):
+        return np.std(numbers)+b+0.5
 
     def summarize(self, dataset, a, b):
-        summaries = [(self.mean(attribute) + a - 0.002, self.stdev(attribute) + b - 0.001) for attribute in zip(*dataset)]
+        summaries = [(self.mean(attribute, a, b), self.stdev(attribute, b)) for attribute in zip(*dataset)]
         return summaries
 
     def summarizeByClass(self, features, classes, a, b):
@@ -81,7 +83,6 @@ class GaussianBayes:
         p_y_x = np.zeros((N, M))
 
         iloczyn = 1
-        sum = 0
 
         for a in range(N):
             for b in range(M):
@@ -90,17 +91,8 @@ class GaussianBayes:
                     x = X[a,c]
                     iloczyn *= self.calculateProbability(x, mean, stdev)
                 iloczyn = iloczyn * p_y[b]
-                sum = sum + iloczyn
+                p_y_x[a, b] = iloczyn
                 iloczyn = 1
-            for b in range(M):
-                for c in range(np.size(X, 1)):
-                    mean, stdev = summaries[float(b)][c]
-                    x = X[a, c]
-                    iloczyn *= self.calculateProbability(x, mean, stdev)
-                iloczyn = iloczyn * p_y[b]
-                p_y_x[a, b] = iloczyn / float(sum)
-                iloczyn = 1
-            sum = 0
         return p_y_x
 
     def error_fun(self, p_y_x, Y):
